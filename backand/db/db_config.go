be945e4548841e3c227067045d4bd334041af5a3
chjcmy <chjcmy@gmail.com>
2021-06-10 21:21:17 +0900

    delete backend add backand

--- backand/db/db_config.go
+++ backand/db/db_config.go
@@ -1,1 +1,19 @@
 package db
+
+import (
+"gorm.io/driver/mysql"
+"gorm.io/gorm"
+)
+
+func Connect() *gorm.DB {
+	dsn := "cshcmi:chltjdgus123!@tcp(choi1994.iptime.org:1994)/blog?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err == nil {
+		println("연결 성공")
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
--- backand/main.go
+++ backand/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 db "backand/db"
-
+	"github.com/gin-gonic/gin"
 )
 
 type Product struct {
@@ -13,5 +13,14 @@ type Product struct {
 func main() {
 	db := db.Connect()
 
-	db.AutoMigrate(&Product{})
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+
+		db.AutoMigrate(&Product{})
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("0.0.0.0:8000")
 }
