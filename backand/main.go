b13c5fe82559b986ccf44fa732f1ed0f13371583
chjcmy <chjcmy@gmail.com>
2021-06-13 21:49:45 +0900

    delete create

--- backand/main.go
+++ backand/main.go
@@ -9,16 +9,8 @@ import (
 func main() {
 	db := db3.Connect()
 
-	db.AutoMigrate( &db3.User{}, &db3.Book{}, &db3.BookSubject{})
+	db.AutoMigrate(&db3.User{}, &db3.Book{}, &db3.BookSubject{})
 
-	//db.Create(&db3.User{
-	//	Model:   gorm.Model{},
-	//	UserName:    "최성현",
-	//	Email:   "chjcmy@gmail.com",
-	//	Github:  "https://github.com/chjcmy",
-	//	Gitlab:  "https://gitlab.com/chjcmy",
-	//	Manager: 0,
-	//})
 	r := gin.Default()
 
 	api := r.Group("/api")
