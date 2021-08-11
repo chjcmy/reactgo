abb0dad13873cb1d67aabdfa945b9eaea561cdba
chjcmy <chjcmy@gmail.com>
2021-08-11 20:03:14 +0900

    edit react

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,8 @@ import SideBar from "./components/common/Sidebar";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Unit from "./components/common/unit/Unit";
 import Home from "./components/common/home/Home";
+import Book from "./components/common/Book/Book";
+import BookWrite from "./components/common/Book/BookWrite";
 
 const App: FC = () => {
     return (
@@ -14,6 +16,8 @@ const App: FC = () => {
                     <Route exact path="/" component={Home}/>
                     <Route path="/unit/:unit" component={Unit}/>
                     <Route path="/all" component={Unit} />
+                    <Route path="/book/:id" component={Book}/>
+                    <Route path="/bookwrite" component={BookWrite} />
                 </Switch>
             </div>
         </Router>
